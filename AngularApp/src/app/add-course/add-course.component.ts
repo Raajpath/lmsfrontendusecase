06b76f2a75e1d23958ec46fa227aa426ba6f88fb
Jha <2048748@cognizant.com>
2024-09-06 15:21:37 +0530

    frontend code of lms

---
+++ AngularApp/src/app/add-course/add-course.component.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { CourseDTO } from '../CourseDTO';
+import { CourseServiceService } from '../service/course-service.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuthenticationService } from '../service/auth/authentication.service';
+
+
+@Component({
+  selector: 'app-add-course',
+  templateUrl: './add-course.component.html',
+  styleUrls: ['./add-course.component.css']
+})
+export class AddCourseComponent implements OnInit {
+
+    course:CourseDTO= new CourseDTO();
+    message=''
+    technologies:string[]=[''];
+    courseResponse:string | undefined;
+    
+  
+    constructor(private router: Router,private courseService:CourseServiceService,public authenticationService: AuthenticationService,private route:ActivatedRoute) { }
+
+    ngOnInit(): void {
+      
+    }
+
+    
+
+    trackByIndex(index:number,item:any):number{
+      return index;
+    }
+
+    addTechnology():void{
+      this.technologies.push('');
+    }
+    removeTechnology(index:number):void{
+      this.technologies.splice(index,1);
+    }
+ 
+    
+    onSubmit(courseForm:NgForm):void{
+      if(courseForm.valid){
+        this.course.technology=this.technologies;
+        console.log('course',this.course);
+        this.courseService.addCourse(this.course).subscribe((response:string)=>{
+          console.log(response);
+          this.courseResponse=response;
+          this.router.navigate(['/lms/get-all-courses'])
+         
+        },error=>{
+          console.log(error);
+          alert("Error occured while adding course!!");
+        });
+      }    
+    }
+
+  
+}
